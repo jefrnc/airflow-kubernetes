1a5fdd82cb25f9066bea5c984b1ce15baf5a06fc
joseph <jsfrnc@gmail.com>
2023-01-04 23:22:57 -0300

    Update get-variable.py

--- get-variable.py
+++ get-variable.py
@@ -1,5 +1,5 @@
 """Get Variable"""
 import logging  
 from airflow.models import Variable   
-my_var = Variable.get('var_name') 
+my_var = Variable.get('my_var') 
 logging.info(f'var_name value: {my_var}')
