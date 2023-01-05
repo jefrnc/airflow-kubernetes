aad167f5c756d879d9eb23b61df26c8eca9e7dea
joseph <jsfrnc@gmail.com>
2023-01-05 12:15:03 -0300

    Update get-variable.py

--- get-variable.py
+++ get-variable.py
@@ -1,5 +1,26 @@
 """Get Variable"""
-import logging  
-from airflow.models import Variable   
-my_var = Variable.get('my_var') 
-logging.info(f'var_name value: {my_var}')
+import json 
+import logging 
+from datetime import datetime 
+from airflow import DAG 
+from airflow.models import Variable 
+from airflow.hooks.base_hook import BaseHook 
+from airflow.operators.python_operator import PythonOperator
+
+def get_variable(**kwargs):
+    conn = BaseHook.get_connection(kwargs['my_conn_id'])
+    my_var = Variable.get(kwargs['var_name']) 
+    logging.info(f'var_name value: {my_var}')
+
+with DAG(   
+    'test_get_variable',    
+    start_date=datetime(2020, 1, 1),    
+    schedule_interval=None
+) as dag:      
+    test_task = PythonOperator(         
+        task_id='test-get_variable',         
+        python_callable=get_variable,         
+        op_kwargs={
+            'var_name': 'my_var',
+        },     
+    )
